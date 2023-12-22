1e017f204d12e3419488f915ccfc3804e9a624f8
Yash <yash456k@gmail.com>
2023-12-22 23:47:03 +0530

    placed app.js inside api folder and renamed it to index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { checkAuth } = require("./middleware/auth");
+const { checkAuth } = require("../middleware/auth");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
-const staticRouter = require("./routes/staticRouter");
-const userRoute = require("./routes/user");
+const staticRouter = require("../routes/staticRouter");
+const userRoute = require("../routes/user");
 
 app.use("/", checkAuth, staticRouter);
 app.use("/user", userRoute);
