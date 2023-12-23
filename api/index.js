313e76affc8e19c2230e7a7d4500a33ae01334f8
Yash <yash456k@gmail.com>
2023-12-23 09:47:31 +0530

    coding garden updates v2

--- api/index.js
+++ api/index.js
@@ -1,23 +1,3 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const path = require("path");
-const { checkAuth } = require("../middleware/auth");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
-
-app.set("view engine", "ejs");
-
-const staticRouter = require("../routes/staticRouter");
-const userRoute = require("../routes/user");
-
-app.use("/", checkAuth, staticRouter);
-app.use("/user", userRoute);
+const app = require("../app");
 
 module.exports = app;
